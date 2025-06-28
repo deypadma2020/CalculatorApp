7d5d4210889204a0f08dc43fc1c9e34c309b652e
deypadma2020 <deypadma2020@gmail.com>
2025-06-28 15:04:41 +0530

    add executable entrypoint to main.py using __name__ == __main__

--- main.py
+++ main.py
@@ -8,3 +8,7 @@ app = FastAPI(
 )
 
 app.include_router(router, prefix="/api/v1")
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
